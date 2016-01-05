0a364711a2be447c9f1c7f22a0034ea7c3e376c4
1401suzanna <szfrnnds24@gmail.com>
2016-01-05 12:38:54 +0530

    version1

---
+++ queue/src/queue/QueueArrayList.java
@@ -0,0 +1,30 @@
+package queue;
+
+import java.util.ArrayList;
+
+public class QueueArrayList implements  QueueInterface{
+
+	ArrayList QArray=new ArrayList();
+	@Override
+	public void Enqueue(int data) {
+		// TODO Auto-generated method stub
+		QArray.add(data);
+		System.out.println("Enqueued Element: "+data);
+		
+	}
+
+	@Override
+	public void Dequeue() {
+		// TODO Auto-generated method stub
+		/* QArray.remove(0);*/
+		 System.out.println("Dequeued Element: "+QArray.remove(0));
+		
+	}
+
+	@Override
+	public void Display() {
+		// TODO Auto-generated method stub
+		System.out.println("Queue= "+QArray);
+	}
+
+}
---
+++ queue/src/queue/QueueInterface.java
@@ -0,0 +1,9 @@
+package queue;
+
+
+public interface QueueInterface {
+
+	public void Enqueue(int data);
+	public void Dequeue();
+	public void Display();
+}
